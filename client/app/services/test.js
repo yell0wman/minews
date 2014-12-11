2a24349cb8f8714ebaedc15bb87fc4d083f74bb6
Anastasia Dzyabura <anastasya.d@myplanetdigital.com>
2014-12-11 11:16:26 +0200

    got data from .xml page - cencor.net

--- client/app/services/test.js
+++ client/app/services/test.js
@@ -5,8 +5,8 @@ angular.module('miNewsApp').
            get: function(callback){
                 console.log('in get');
 //                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/find", { params: { "v": "1.0", "q": "tsn.ua/rss", "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
-//                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/find", { params: { "v": "1.0", "q": "censor.net.ua/includes/news_ru.xml", "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
-                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/find", { params: { "v": "1.0", "q": "habrahabr.ru/rss/hubs/", "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
+//                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/find", { params: { "v": "1.0", "q": "habrahabr.ru/rss/hubs/", "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
+                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://censor.net.ua/includes/news_ru.xml", "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
             .success(function(data) {
               console.log(data);
             })
