3005d7b41d63a6758e87c0649b185c443c540034
Anastasia Dzyabura <anastasya.d@myplanetdigital.com>
2014-12-11 19:02:27 +0200

    minews-2: news from different sites are displayed in random order

--- client/app/controllers/main.js
+++ client/app/controllers/main.js
@@ -1,16 +1,58 @@
 angular.module('miNewsApp')
   .controller('MainCtrl', ['$scope', 'GetFeeds', function($scope, GetFeeds) {
       
+      sourses = ["censor.net.ua/includes/news_ru.xml", "habrahabr.ru/rss/hubs/", "tsn.ua/rss"];
+      oneSiteFeeds = [];
+      
+      // Request callback
       setFeeds = function(data) {
         if (data.responseData.entries) {
-          $scope.feeds = data.responseData.entries;
+          data.responseData.entries.forEach(function(element, index, array) {
+            oneSiteFeeds.push(element);
+          });
         }
         else {
-          $scope.feeds = data.responseData.feed.entries;
+          data.responseData.feed.entries.forEach(function(element, index, array) {
+            oneSiteFeeds.push(element);
+          });
         }
+          $scope.feeds = shuffle(oneSiteFeeds);
       };
       
-      GetFeeds.get(setFeeds);
+      // Mix array elements
+      shuffle = function(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
 
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+      };
+      
+      // Set values for GetFeeds.get() parameters
+      sourses.forEach(function(element, index, array) {
+        if (element.indexOf('.xml', -4)) {
+          prefix = 'http://';
+          googleApiSuffix = 'load';
+        } else {
+          prefix = '';
+          googleApiSuffix = 'find';
+        }
+        
+      // Get news by rss
+      GetFeeds.get(prefix + element, googleApiSuffix, setFeeds);
+        
+      });
+      
   }]);
   
--- client/app/services/main.js
+++ client/app/services/main.js
@@ -2,10 +2,8 @@ angular.module('miNewsApp').
     factory('GetFeeds', ['$http', function($http){
         
        return {
-           get: function(callback){
-//                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/find", { params: { "v": "1.0", "q": "tsn.ua/rss", "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
-//                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/find", { params: { "v": "1.0", "q": "habrahabr.ru/rss/hubs/", "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
-                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://censor.net.ua/includes/news_ru.xml", "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
+           get: function(sourse, googleApiSuffix, callback){
+                 $http.jsonp("http://ajax.googleapis.com/ajax/services/feed/"+googleApiSuffix, { params: { "v": "1.0", "q": sourse, "alt": "json-in-script", "callback": "JSON_CALLBACK" } })
             .success(function(data) {
               callback(data);
             })
